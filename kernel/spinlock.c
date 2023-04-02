09f6b0d1971e989220aff02655f460b66fec9130
Ron Levi <ronlv4@gmail.com>
2023-04-02 13:23:07 +0000

    restore spinlock without backtrace

--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -23,10 +23,7 @@ acquire(struct spinlock *lk)
 {
   push_off(); // disable interrupts to avoid deadlock.
   if(holding(lk))
-  {
-    printf("proc name - %s\n", myproc()->name);
     panic("acquire");
-  }
 
   // On RISC-V, sync_lock_test_and_set turns into an atomic swap:
   //   a5 = 1
